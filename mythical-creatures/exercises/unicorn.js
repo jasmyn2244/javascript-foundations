064e649a5617fd5c913f65609063a1c47310b8cd
Jasmyn Hicks <jasmynh44@gmail.com>
2021-10-19 21:17:54 -0600

    Complete unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,19 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = "white") {
     this.name = name;
     this.color = color;
   }
+
+isWhite() {
+  if (this.color !== "white") {
+    return false
+  }
 }
+
+says(unicornQuote) {
+  return `**;* ${unicornQuote} *;**`
+}
+}
+
 
 module.exports = Unicorn;
